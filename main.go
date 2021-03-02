b801afe6ef7fb9c5289f1fe339b39e7fb3982c03
Kirk Fletcher <kirkfletcher@Kirks-iMac-Pro.fritz.box>
2021-03-02 09:12:35 +0000

    initial commit

---
+++ main.go
@@ -0,0 +1,453 @@
+package querybuilder
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Sqlbuilder struct {
+	string string
+	selectStmt string
+	whereStmt string
+	whereinStmt string
+	fromStmt string
+	deletefromStmt string
+	leftjoinStmt string
+	limitStmt string
+	offsetStmt string
+	orderbyStmt string
+	Dialect string //Can be postgres, mysql or (more to come)
+}
+
+func (s *Sqlbuilder) From(fromStmt string) *Sqlbuilder {
+	s.fromStmt = s.formatSchema(fromStmt)
+
+	return s
+}
+
+func (s *Sqlbuilder) DeleteFrom(fromStmt string) *Sqlbuilder {
+	s.deletefromStmt = s.formatSchema(fromStmt)
+
+	return s
+}
+
+func (s *Sqlbuilder) SelectRaw(selectStmt string) *Sqlbuilder {
+	re := regexp.MustCompile(`\r?\n`)
+	selectStmt = re.ReplaceAllString(selectStmt, " ")
+
+	s.selectStmt += selectStmt + `, `
+	return s
+}
+
+func (s *Sqlbuilder) Select(selectStmt ...string) *Sqlbuilder {
+
+	for _, ss := range selectStmt {
+		s.selectStmt += s.formatSchema(ss) + `, `
+	}
+
+	return s
+}
+
+func (s *Sqlbuilder) Where(table string, operator string, value string) *Sqlbuilder {
+
+	operator = strings.ToUpper(operator)
+	value = strings.TrimSuffix(value, `'`)
+	value = strings.TrimSuffix(value, `"`)
+	value = strings.TrimSuffix(value, "`")
+	value = strings.TrimPrefix(value, `'`)
+	value = strings.TrimPrefix(value, `"`)
+	value = strings.TrimPrefix(value, "`")
+
+	switch operator {
+	case `BETWEEN`:
+		re := regexp.MustCompile("and|AND|And")
+		vp := re.Split(value, -1)
+
+		value = ``
+
+		for _, v := range vp {
+			value += sanitiseString(`'` + strings.TrimSpace(v) + `'`) + ` AND `
+		}
+
+		value = strings.TrimSuffix(value, ` AND `)
+	default:
+		value = sanitiseString(`'` + value + `'`)
+	}
+
+	s.whereStmt += s.formatSchema(table) + " " + operator + " " + value + ` AND `
+
+	return s
+}
+
+func (s *Sqlbuilder) WhereRaw(whereStmt string) *Sqlbuilder {
+	s.whereStmt += whereStmt + ` AND `
+	return s
+}
+
+//Accepts Slice of INT, FLOAT32, STRING, or a simple comma separated STRING
+func (s *Sqlbuilder) WhereIn(column string, params interface{}) *Sqlbuilder {
+
+	output := ""
+
+	switch foo := params.(type) {
+	case []int, []float32:
+		output += "(" + strings.Trim(strings.Join(strings.Fields(fmt.Sprint(foo)), ", "), "[]") + ")"
+		break
+	case []string:
+		output += "("
+		for _, v := range foo {
+			output += "'" + sanitiseString(v) + "', "
+		}
+		output = strings.TrimSuffix(output, ", ")
+		output += ")"
+		break
+	case string:
+		output = "(" + sanitiseString(foo) + ")"
+		break
+	default:
+		output = ""
+	}
+
+	if output != "" {
+		s.WhereRaw(column + ` IN ` +  output)
+	}
+
+	return s
+}
+
+func (s *Sqlbuilder) WhereStringMatchAny(column string, params []string) *Sqlbuilder {
+
+	output := ""
+
+	output += "(array["
+	for _, v := range params {
+		output += "'%" + sanitiseString(strings.TrimSpace(v)) + "%', "
+	}
+	output = strings.TrimSuffix(output, ", ")
+	output += "])"
+
+	if output != "" {
+		s.WhereRaw(column + ` ILIKE ANY ` + output)
+	}
+
+	return s
+}
+
+func (s *Sqlbuilder) WhereStringMatchAll(column string, params []string) *Sqlbuilder {
+
+	output := ""
+
+	output += "'"
+	for _, v := range params {
+		output += "%" + sanitiseString(strings.TrimSpace(v)) + "% "
+	}
+	output = strings.TrimSuffix(output, " ")
+	output += "'"
+
+	if output != "" {
+		s.WhereRaw(column + ` ILIKE ` + output)
+	}
+
+	return s
+}
+
+func (s *Sqlbuilder) LeftJoin(table string, as string, on string) *Sqlbuilder {
+
+	table = s.formatSchema(table)
+	on = s.formatJoinOn(on)
+	as = s.formatSchema(as)
+
+	s.leftjoinStmt += `LEFT JOIN ` + table + ` AS ` + as + ` ON ` + on + ` `
+	return s
+}
+
+func (s *Sqlbuilder) LeftJoinExtended(table string, as string, on string, additionalQuery string) *Sqlbuilder {
+
+	table = s.formatSchema(table)
+	on = s.formatJoinOn(on)
+
+	s.leftjoinStmt += `LEFT JOIN ` + table + ` AS "` + as + `" ON ` + on + ` ` + additionalQuery + ` `
+	return s
+}
+
+func (s *Sqlbuilder) Limit(limit int) *Sqlbuilder {
+	s.limitStmt = `LIMIT ` + strconv.Itoa(limit) + ` `
+
+	return s
+}
+
+func (s *Sqlbuilder) Offset(offset int) *Sqlbuilder {
+	s.offsetStmt = `OFFSET ` + strconv.Itoa(offset) + ` `
+
+	return s
+}
+
+func (s *Sqlbuilder) OrderBy(column string, diretion string) *Sqlbuilder {
+
+	s.orderbyStmt = `ORDER BY "` + column + `" ` + diretion
+
+	return s
+}
+
+func (s *Sqlbuilder) Reset() *Sqlbuilder {
+	s.string = ``
+	s.selectStmt = ``
+	s.orderbyStmt = ``
+	s.whereinStmt = ``
+	s.limitStmt = ``
+	s.fromStmt = ``
+	s.leftjoinStmt = ``
+	s.whereStmt = ``
+	s.offsetStmt = ``
+
+	return s
+}
+
+func (s *Sqlbuilder) Count() string {
+	sqlquery := s.Build()
+
+	countQuery := `SELECT COUNT(*) AS rowcount FROM (` + sqlquery + `) AS rowdata`
+
+	return countQuery
+}
+
+func (s *Sqlbuilder) Exists() string {
+	sqlquery := s.Build()
+
+	existsQuery := `SELECT EXISTS (` + sqlquery + `)`
+
+	return existsQuery
+}
+
+func (s *Sqlbuilder) Build() string {
+
+	//build selects
+	if s.deletefromStmt == `` {
+		if s.selectStmt == `` {
+			s.string = `SELECT * `
+		} else {
+			s.string = `SELECT ` + strings.TrimSuffix(s.selectStmt, `, `) + ` `
+		}
+	}
+
+	//build from
+	if s.fromStmt == `` {
+		if s.deletefromStmt != `` {
+			s.string += `DELETE FROM ` + strings.TrimSuffix(s.deletefromStmt, `.`) + ` `
+		}else{
+			return ``
+		}
+	}else {
+		s.string += `FROM ` + strings.TrimSuffix(s.fromStmt, `.`) + ` `
+	}
+
+
+	//left joins
+	s.string += s.leftjoinStmt + ` `
+
+
+	//where
+	if s.whereStmt != `` {
+		s.string += `WHERE ` + strings.TrimSuffix(s.whereStmt, ` AND `) + ` `
+	}
+
+
+	//orderby
+	if s.orderbyStmt != `` {
+		s.string += s.orderbyStmt + ` `
+	}
+
+
+	//limit and offset
+	s.string += s.limitStmt
+	s.string += s.offsetStmt
+
+	space := regexp.MustCompile(`\s+`)
+	s.string = space.ReplaceAllString(s.string, " ")
+
+	returnString := s.string
+
+	return returnString
+}
+
+
+func (s *Sqlbuilder) BuildInsert(table string, data interface{}, additionalQuery string) (string, error) {
+	dbCols, dbVals, err := mapStruct(data)
+	if err != nil {
+		return "", err
+	}
+
+	sql := "INSERT INTO " + s.formatSchema(table) + " (" + strings.Join(dbCols, ", ") + ") VALUES (" + strings.Join(dbVals, ", ") + ") " + additionalQuery
+
+	return sql, nil
+}
+
+
+func (s *Sqlbuilder) BuildUpdate(table string, data interface{}, additionalQuery string) (string, error) {
+
+	dbCols, dbVals, err := mapStruct(data)
+	if err != nil {
+		return "", err
+	}
+
+	setString := ""
+	sql := ""
+
+	for i, col := range dbCols {
+		setString += col + ` = ` + dbVals[i] + `, `
+	}
+	setString = strings.TrimSuffix(setString, `, `) + ` `
+
+	if setString != ""{
+		sql = "UPDATE " + s.formatSchema(table) + ` SET ` + setString
+
+		if s.whereStmt != `` {
+			sql += `WHERE ` + strings.TrimSuffix(s.whereStmt, ` AND `) + ` `
+		}
+
+		sql += additionalQuery
+
+		return sql, nil
+	}
+
+	return sql, errors.New("sql build failed")
+}
+
+
+
+
+
+/**
+Helpers
+*/
+
+func (s *Sqlbuilder) formatSchema (schema string) string {
+	schemaParts := strings.Split(schema, ".")
+	finalSchemaStmt := ``
+
+	var dialectFormat string
+
+	switch strings.ToLower(s.Dialect) {
+	case "postgres":
+		dialectFormat = `"`
+		break
+	case "mysql":
+		dialectFormat = "`"
+		break
+	default:
+		dialectFormat = `"`
+	}
+
+
+	for _, v := range schemaParts {
+		if v == `*` {
+			finalSchemaStmt += `*`
+		}else{
+			part := strings.TrimSpace(v)
+			if string(part[0]) == dialectFormat && string(part[len(part) - 1]) == dialectFormat {
+				finalSchemaStmt += part + `.`
+			}else{
+				finalSchemaStmt += dialectFormat + part + dialectFormat + `.`
+			}
+
+		}
+	}
+
+	return strings.TrimSuffix(finalSchemaStmt, `.`)
+}
+
+func (s *Sqlbuilder) formatJoinOn (joinStmt string) string {
+	joinParts := strings.Split(joinStmt, "=")
+	finalJoinStmt := ``
+
+	for _, v := range joinParts {
+		finalJoinStmt += s.formatSchema(v) + ` = `
+	}
+
+	return strings.TrimSuffix(finalJoinStmt, ` = `)
+}
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap   = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func toSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake  = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+func mapStruct (data interface{}) (dbCols []string, dbVals []string, error error){
+	fields := reflect.TypeOf(data)
+	values := reflect.ValueOf(data)
+
+	num := fields.NumField()
+
+	for i := 0; i < num; i++ {
+		field := fields.Field(i)
+		value := values.Field(i)
+
+		val, exists := field.Tag.Lookup("sqlb")
+
+		if exists {
+			dbCols = append(dbCols, "\"" + val + "\"")
+		}else{
+			dbCols = append(dbCols,  "\"" + toSnakeCase(field.Name)  + "\"")
+		}
+
+		var v string
+
+		switch value.Kind() {
+		case reflect.String:
+			v = "'" + sanitiseString(value.String()) + "'"
+		case reflect.Int:
+			v = strconv.FormatInt(value.Int(), 10)
+		case reflect.Int8:
+			v = strconv.FormatInt(value.Int(), 10)
+		case reflect.Int32:
+			v = strconv.FormatInt(value.Int(), 10)
+		case reflect.Int64:
+			v = strconv.FormatInt(value.Int(), 10)
+		case reflect.Float64:
+			v = fmt.Sprintf("%f", value.Float())
+		case reflect.Float32:
+			v = fmt.Sprintf("%f", value.Float())
+		case reflect.Bool:
+			if value.Bool(){
+				v = "TRUE"
+			}else{
+				v = "FALSE"
+			}
+		default:
+			return dbCols, dbVals, errors.New("type: " + value.Kind().String() + " unsupported")
+		}
+
+		dbVals = append(dbVals, v)
+	}
+
+	return dbCols, dbVals, nil
+}
+
+func sanitiseString(str string) string {
+
+	if len(str) > 0 {
+		rebuildSingles := false
+
+		if string(str[0]) == "'" && string(str[len(str) - 1]) == "'" {
+			rebuildSingles = true
+		}
+
+
+		str = strings.TrimSuffix(strings.TrimPrefix(str, "'"), "'")
+		str = strings.ReplaceAll(str, "'", "''")
+
+		if rebuildSingles {
+			str = "'" + str + "'"
+		}
+	}
+
+	return str
+}
